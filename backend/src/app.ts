9b0a7d84887ec5fcd1c0445cc4ae5cd937ba5be9
Diego Antonio <suporte.diegociara@gmail.com>
2025-05-21 13:45:55 -0300

    Update: limit payload

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,8 @@ const app = express();
 
 import './database';
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors());
 app.use(routes);
 
