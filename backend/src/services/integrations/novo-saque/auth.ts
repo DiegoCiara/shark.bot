41ae3114d7ecf3ef76b645cca614b7fc7bd91d64
Diego Antonio <suporte.diegociara@gmail.com>
2025-05-21 17:53:29 -0300

    Update test mychat

--- backend/src/services/integrations/novo-saque/auth.ts
+++ backend/src/services/integrations/novo-saque/auth.ts
@@ -8,18 +8,18 @@ const novoSaqueUrl = process.env.NOVOSAQUE_API_URL;
 export async function authenticateNovoSaque(): Promise<any> {
   try {
     const response = await axios.post(`${novoSaqueUrl}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'accept': 'application/json',
-      },
       body: {
         email: process.env.NOVOSAQUE_EMAIL,
         password: process.env.NOVOSAQUE_PASSWORD,
       },
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'accept': 'application/json',
+      },
     });
 
-    return response.data.token;
+    return response;
   } catch (error) {
     console.error('Error:', error);
   }
