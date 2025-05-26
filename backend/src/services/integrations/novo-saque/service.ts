7633e1714bdb6c78c99bdacb8539520cd364839d
Diego Antonio <suporte.diegociara@gmail.com>
2025-05-26 19:18:35 -0300

    update

--- backend/src/services/integrations/novo-saque/service.ts
+++ backend/src/services/integrations/novo-saque/service.ts
@@ -31,8 +31,8 @@ export async function service(args: any, token?: string): Promise<any> {
         phone: '(86) 99562-5928',
         phone_store: '(86) 99562-5928',
         phone_seler: '(86) 99562-5928',
-        simulation_fgts: products,
-        simularion_json: target,
+        simulation_fgts: JSON.stringify(products),
+        simularion_json: JSON.stringify(target),
         has_secure: false,
         liquid_value: liquid_value,
         released_amount: liquid_value, // response -> LiquidValue
